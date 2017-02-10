2d25145b56bfa6b05cc388363ae6c955ddd7ea07
yktang <tyingkai@gmail.com>
2017-02-10 15:01:19 +0800

    initial commit

---
+++ src/main/java/com/weiwei/IprServerApplication.java
@@ -0,0 +1,12 @@
+package com.weiwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IprServerApplication.class, args);
+	}
+}
