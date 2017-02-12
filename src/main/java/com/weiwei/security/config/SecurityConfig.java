7f8c79116116c468c4b47ed7987dcb398ca78637
yktangint <tang.yingkai@intellif.com>
2017-02-12 11:39:55 +0800

    updates

--- src/main/java/com/weiwei/security/config/SecurityConfig.java
+++ src/main/java/com/weiwei/security/config/SecurityConfig.java
@@ -3,14 +3,15 @@ package com.weiwei.security.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
-@Profile("customquery")
+//@Profile("customquery")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
